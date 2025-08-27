a87577a39de0e2da615e1a2665b15cbec750a2e3
MohammedAdnan <contactadnan19@gmail.com>
2025-08-27 16:48:30 -0700

    Ready for Render deployment

--- config/db.js
+++ config/db.js
@@ -1,10 +1,28 @@
-const mysql = require("mysql2/promise");
+// const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Adnan@2546",
-  database: "school_db",
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "ab@124",
+//   database: "school_db",
+// });
+
+// module.exports = pool;
+// =======================================
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool;
+db.connect((err) => {
+  if (err) console.error("DB connection failed:", err);
+  else console.log("Connected to Railway DB");
+});
+
+module.exports = db;
